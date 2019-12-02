ed8c26b1662ad5b1452485d3e15621ca2ce97eac
Stanislav Klyukhin <stanislavak314@gmail.com>
2019-12-02 17:13:05 +0600

    Create performance view

---
+++ Sources/PerformanceMonitor/PerformanceView.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright © 2019 Rosberry. All rights reserved.
+//
+
+import UIKit
+
+final class PerformanceView: UIView {
+
+    // MARK: - Subviews
+
+    private lazy var memoryUsageLabel: UILabel = .init()
+
+    // MARK: - Lifecycle
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+    // MARK: - Private
+}
