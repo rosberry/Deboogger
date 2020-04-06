72ca792bc81517625debd05d87218666354ad4b4
Stanislav Klyukhin <stanislavak314@gmail.com>
2020-04-06 17:19:47 +0600

    Add text field to slider cell

--- Sources/Cells/SliderTableViewCell/SliderTableViewCell.swift
+++ Sources/Cells/SliderTableViewCell/SliderTableViewCell.swift
@@ -6,7 +6,15 @@
 import UIKit
 
 final class SliderTableViewCell: BaseTableViewCell {
+
+    private enum Constants {
+        static let sliderHeight: CGFloat = 40
+        static let textFieldHeight: CGFloat = 40
+    }
+
     private var plugin: SliderPlugin?
+
+    private lazy var accessoryContainerView: UIView = .init()
 
     private lazy var sliderView: UISlider = {
         let view = UISlider()
@@ -14,11 +22,23 @@ final class SliderTableViewCell: BaseTableViewCell {
         return view
     }()
 
+    private lazy var textField: UITextField = {
+        let view = UITextField()
+        view.keyboardType = .decimalPad
+        view.borderStyle = .line
+        view.placeholder = "Enter manually"
+        view.delegate = self
+        view.addTarget(self, action: #selector(textFieldValueChanged), for: .valueChanged)
+        return view
+    }()
+
     // MARK: -
 
     override func setup() {
         super.setup()
-        accessoryView = sliderView
+        accessoryContainerView.addSubview(sliderView)
+        accessoryContainerView.addSubview(textField)
+        accessoryView = accessoryContainerView
     }
 
     override func configure(with plugin: Plugin) {
@@ -35,8 +55,15 @@ final class SliderTableViewCell: BaseTableViewCell {
     }
 
     override func layoutSubviews() {
-        sliderView.bounds = CGRect(x: 0.0, y: 0.0, width: bounds.midX, height: 40.0)
+        let containerViewHeight = Constants.sliderHeight + Constants.textFieldHeight
+        accessoryContainerView.bounds = CGRect(x: 0.0, y: 0.0, width: bounds.midX, height: containerViewHeight)
+        sliderView.frame = CGRect(x: 0.0, y: 0.0, width: bounds.midX, height: Constants.sliderHeight)
+        textField.frame = CGRect(x: 0.0, y: sliderView.frame.maxY, width: bounds.midX, height: Constants.textFieldHeight)
         super.layoutSubviews()
+    }
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return .init(width: size.width, height: Constants.sliderHeight + Constants.textFieldHeight)
     }
 
     //MARK: - Actions
@@ -47,8 +74,25 @@ final class SliderTableViewCell: BaseTableViewCell {
         plugin?.sliderValueChanged(sender)
     }
 
+    @objc private func textFieldValueChanged(_ sender: UITextField) {
+        guard let text = sender.text, let value = Float(text), text.isEmpty == false else {
+            return
+        }
+        sliderView.setValue(value, animated: true)
+    }
+
+    // MARK: - Private
+
     private func configureSliderTitle() {
         detailTextLabel?.attributedText = plugin?.description
         setNeedsLayout()
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension SliderTableViewCell: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        endEditing(true)
     }
 }
